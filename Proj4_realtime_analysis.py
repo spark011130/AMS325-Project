a6d044d849eecfcb58fd81cc59381b3650df8ce7
spark_1130 <phkmath0@gmail.com>
2024-12-13 03:25:15 -0500

    RealTime Analysis Completion

--- Proj4_realtime_analysis.py
+++ Proj4_realtime_analysis.py
@@ -1,35 +1,88 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from tensorflow.keras.models import load_model
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+from Proj2_golden_ratio_distance_processing import Parameter_By_2, Parameter_Continuous, average_position
 
-model = load_model('trained_model.h5')
+def main():
+    model = load_model('trained_model.keras')
+    mp_face_mesh = mp.solutions.face_mesh
+    mp_drawing = mp.solutions.drawing_utils
+    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-for layer in model.layers:
-    weights = layer.get_weights()  # weights[0]: 가중치, weights[1]: 편향
-    if weights:
-        print(f'레이어: {layer.name}, 가중치 형태: {weights[0].shape}')
+    face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True)
+    cap = cv2.VideoCapture(0)
+    
+    if not cap.isOpened():
+        print("Camera not available.")
+        return
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't read frame.")
+            return
+        rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = face_mesh.process(rgb_image)
+
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1)
+                )
+        cv2.imshow('live camera', frame)
         
+        key = cv2.waitKey(1) & 0xFF
         
-# 특정 레이어의 가중치 추출
-layer_name = 'dense'  # 예: 'dense', 'dense_1' 등
-layer = model.get_layer(name=layer_name)
-weights, biases = layer.get_weights()
+        if key == ord('r'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    
+    h, w, _ = frame.shape
+    landmarks = [(int(lm.x * w), int(lm.y * h)) for lm in results.multi_face_landmarks[0].landmark]
+    
+    vertical = Parameter_Continuous([
+        landmarks[10],  # Forehead
+        landmarks[9],   # Mid eyebrow
+        landmarks[2],   # Under the nose
+        landmarks[17],  # Lower lip
+        landmarks[152]  # Chin
+    ])
 
-# 히스토그램으로 가중치 분포 시각화
-plt.figure(figsize=(8, 4))
-plt.hist(weights.flatten(), bins=30, color='blue', alpha=0.7)
-plt.title(f'{layer_name} 레이어 가중치 분포')
-plt.xlabel('가중치 값')
-plt.ylabel('빈도')
-plt.savefig(f'{layer_name}_weights_histogram.png')
-plt.close()
+    # five parameters
+    horizontal = Parameter_Continuous([
+        average_position(landmarks[162], landmarks[127]),  # Left face end
+        landmarks[33],   # Left eye left
+        landmarks[133],  # Left eye right
+        landmarks[362],  # Right eye left
+        landmarks[263], # Righy eye right
+        average_position(landmarks[389], landmarks[356]) # Left face end
+    ])
 
-# 히트맵으로 가중치 시각화
-plt.figure(figsize=(10, 8))
-sns.heatmap(weights, cmap='viridis')
-plt.title(f'{layer_name} 레이어 가중치 히트맵')
-plt.xlabel('입력 노드')
-plt.ylabel('출력 노드')
-plt.savefig(f'{layer_name}_weights_heatmap.png')
-plt.close()
+    # Parameter_By_2 classes
+    # two parameters
+    nose_to_lip = Parameter_By_2([
+        [landmarks[279], landmarks[49]], # Nose horizontal length
+        [landmarks[291], landmarks[61]] # Lip horizontal length
+    ])
+
+    # two parameters
+    upper_lower_face = Parameter_By_2([
+        [average_position(landmarks[162], landmarks[127]),
+        average_position(landmarks[389], landmarks[356])], # [Left face end, Right face end]
+        [landmarks[397], 
+        landmarks[172]] # [Chin right, Chin left]
+    ])
+
+    parameters = np.concatenate((vertical.ratio, horizontal.ratio, nose_to_lip.ratio, upper_lower_face.ratio))
+    parameters_reshaped = parameters.reshape(1, -1)
+    y_pred = model.predict(parameters_reshaped).flatten()
+    print(y_pred)
+    
+if __name__ == "__main__":
+    main()
